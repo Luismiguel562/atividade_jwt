aee268d3e1b3446d73d52504522a7d7c8c2be2b4
Luis <981624703luis@gmail.com>
2025-05-11 22:01:10 -0300

    alteração

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,9 +1,8 @@
-State } from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { TextField, Button, Container, Typography, Card, CardContent, Box } from '@mui/material'
 import axios from 'axios'
 import { useAuth } from '../utils/AuthContext'
-import api from '../utils/axiosInstance'
 
 export default function LoginPage() {
     const [username, setUsername] = useState('mor_2314')
@@ -13,28 +12,27 @@ export default function LoginPage() {
 
     const handleLogin = async () => {
         try {
-        const res = await api.post('/auth/login', {
-            username,
-            password,
-        })
-        setToken(res.data.token)
-        console.log(res)
-        console.log(`Token: ${res.data.token}`)
-        navigate('/produtos')
+            const res = await axios.post('https://fakestoreapi.com/auth/login', {
+                username,
+                password,
+            })
+            setToken(res.data.token)
+            console.log(res)
+            console.log(`Token: ${res.data.token}`)
+
+            navigate('/produtos')
         } catch (e) {
-        alert('Login fail')
-        console.error(`Login fail:\n${e}`)
+            alert('Login fail')
+            console.error(`Login fail:\n${e}`)
         }
     }
 
     return (
-        <Container maxWidth="sm" sx={{ 
-                marginTop: '6vw', 
-            }}>
+        <Container maxWidth="sm" sx={{ marginTop: '6vw' }}>
             <Card sx={{
-            borderRadius: '16px',
-            boxShadow: '0px 0px 14px rgba(0, 0, 0, 0.37)',
-            paddingY: '12px'
+                borderRadius: '16px',
+                boxShadow: '0px 0px 14px rgba(0, 0, 0, 0.37)',
+                paddingY: '12px'
             }}>
                 <CardContent>
                     <Typography variant="h1" align='center' sx={{
@@ -43,43 +41,60 @@ export default function LoginPage() {
                     }}>Login</Typography>
 
                     <Box display="flex" justifyContent="center" marginTop={'32px'}>
-                        <TextField label="user" value={username} onChange={(e) => setUsername(e.target.value)} sx={{
-                            borderRadius: '12px',
-                            boxShadow: '0px 0px 14px rgba(0, 0, 0, 0.37)',
-                            transformOrigin: 'center',
-                            '& .MuiOutlinedInput-root': {
+                        <TextField
+                            label="user"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            sx={{
                                 borderRadius: '12px',
-                                fontSize: '1.2rem',
-                                width: '370px'
-                            }
-                        }}/>    
+                                boxShadow: '0px 0px 14px rgba(0, 0, 0, 0.37)',
+                                '& .MuiOutlinedInput-root': {
+                                    borderRadius: '12px',
+                                    fontSize: '1.2rem',
+                                    width: '370px'
+                                }
+                            }}
+                        />
                     </Box>
 
                     <Box display="flex" justifyContent="center" marginTop={'22px'}>
-                        <TextField label="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} sx={{
-                            borderRadius: '12px',
-                            boxShadow: '0px 0px 14px rgba(0, 0, 0, 0.37)',
-                            '& .MuiOutlinedInput-root': {
+                        <TextField
+                            label="password"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            sx={{
                                 borderRadius: '12px',
-                                fontSize: '1.2rem',
-                                width: '370px'
-                            }
-                        }}/>
+                                boxShadow: '0px 0px 14px rgba(0, 0, 0, 0.37)',
+                                '& .MuiOutlinedInput-root': {
+                                    borderRadius: '12px',
+                                    fontSize: '1.2rem',
+                                    width: '370px'
+                                }
+                            }}
+                        />
                     </Box>
 
                     <Box display="flex" justifyContent="center" marginTop={'32px'}>
-                        <Button variant="contained" onClick={handleLogin} sx={{
-                            backgroundColor: '#b388ff',
-                            '&:hover': {
-                                backgroundColor: '#9c66ff'
-                            },
-                            borderRadius: '32px',
-                            boxShadow: '0px 0px 14px rgba(0, 0, 0, 0.37)',
-                            paddingX: '36px',
-                            fontSize: '1.3rem'
-                        }}>LOGIN</Button>
+                        <Button
+                            variant="contained"
+                            onClick={handleLogin}
+                            sx={{
+                                backgroundColor: '#b388ff',
+                                '&:hover': {
+                                    backgroundColor: '#9c66ff'
+                                },
+                                borderRadius: '32px',
+                                boxShadow: '0px 0px 14px rgba(0, 0, 0, 0.37)',
+                                paddingX: '36px',
+                                fontSize: '1.3rem'
+                            }}
+                        >
+                            LOGIN
+                        </Button>
                     </Box>
                 </CardContent>
             </Card>
         </Container>
     )
+}
